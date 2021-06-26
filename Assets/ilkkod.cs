7ac9e5418781539a9a065ee61bef8bdc35cfe879
ekordo <ekordo2@gmail.com>
2021-06-26 06:45:29 +0300

    ilk kod

---
+++ Assets/ilkkod.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace egitim
+{
+    public class ilkkod : MonoBehaviour
+    {
+        public int pblc = 0;
+        [SerializeField] int srlz = 0;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
