c0980291fba102d6199f285cd1a6f3c70a73252a
ekordo <ekordo2@gmail.com>
2021-06-29 06:10:49 +0300

    rigidbody

---
+++ Assets/Scripts/ControlRigidbody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace lesson
+{
+    public class ControlRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidbodySettings _settings;
+
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_settings.JumpForce,ForceMode.Impulse);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/ControlRigidbodySettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace lesson
+{
+    [CreateAssetMenu(menuName = "Lesson/lesson1/Control Rigidbody Settings")]
+    public class ControlRigidbodySettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _jumpForce;
+        public Vector3 JumpForce{get{return _jumpForce;}}
+
+    }
+}
